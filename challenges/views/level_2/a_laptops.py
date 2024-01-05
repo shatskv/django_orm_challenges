0a6483b7277ae84ab0e340cb25d3a70e8bd527d2
Vladimir Shatskiy <vovaysach@gmail.com>
2024-01-06 02:04:22 +0300

    fix errors in view level_2

--- challenges/views/level_2/a_laptops.py
+++ challenges/views/level_2/a_laptops.py
@@ -14,18 +14,37 @@
 import json
 
 from django.db.models import QuerySet
-from django.http import (HttpRequest, HttpResponse, HttpResponseForbidden,
-                         HttpResponseNotFound)
+from django.http import HttpRequest, JsonResponse
+from django.core.exceptions import ValidationError
 
 from challenges.models import Laptop
+from typing import Any
 
 
-def get_dict_for_queryset(queryset: QuerySet):
+def get_dict_for_queryset(queryset: QuerySet) -> list[dict[str, Any]]:
     data = [item.to_json() for item in queryset]
+
     return data
 
 
-def laptop_details_view(request: HttpRequest, laptop_id: int) -> HttpResponse:
+def validate_price_and_brand(params: dict[str, Any]) -> None:
+    brand = params.get('brand')
+    min_price = params.get('min_price')
+
+    if not brand:
+        raise ValidationError('parameter "brand" is missing')
+    if not min_price:
+        raise ValidationError('parameter "min_price" is missing')
+    
+    try:
+        price = int(min_price)
+    except ValueError:
+        raise ValidationError('parametr "min_price" is not number')
+    if price < 0:
+        raise ValidationError('parametr "min_price" should be > 0')
+
+
+def laptop_details_view(request: HttpRequest, laptop_id: int) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть json-описание ноутбука по его id.
     Если такого id нет, вернуть 404.
@@ -33,43 +52,51 @@ def laptop_details_view(request: HttpRequest, laptop_id: int) -> HttpResponse:
     try:
         laptop = Laptop.objects.get(pk=laptop_id)
     except Laptop.DoesNotExist:
-        return HttpResponseNotFound("This laptop doesn't exist")
-    return HttpResponse(json.dumps(laptop.to_json()))
+        return JsonResponse({"error": {'code': 'not_found', 'message': 'laptop not found'}}, status=404)
+    return JsonResponse(laptop.to_json())
 
 
-def laptop_in_stock_list_view(request: HttpRequest) -> HttpResponse:
+def laptop_in_stock_list_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть json-описание всех ноутбуков, которых на складе больше нуля.
     Отсортируйте ноутбуки по дате добавления, сначала самый новый.
     """
-    laptops = Laptop.objects.filter(quantity__gt=0).order_by('-created_at').all()
-    return HttpResponse(json.dumps(get_dict_for_queryset(laptops)))
+    laptops = Laptop.objects.filter(quantity__gt=0).order_by('-created_at')
+    return JsonResponse(get_dict_for_queryset(laptops), safe=False)
 
 
-def laptop_filter_view(request: HttpRequest) -> HttpResponse:
+def laptop_filter_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть список ноутбуков с указанным брендом и указанной минимальной ценой.
     Бренд и цену возьмите из get-параметров с названиями brand и min_price.
     Если бренд не входит в список доступных у вас на сайте или если цена отрицательная, верните 403.
     Отсортируйте ноутбуки по цене, сначала самый дешевый.
     """
-    brand = request.GET.get('brand')
-    min_price = request.GET.get('min_price')
-    if not all([brand, min_price]):
-        return HttpResponseForbidden('Some reqired parameters are missing!')
-    laptops = Laptop.objects.filter(brand=brand, price__gte=min_price).all()
+    params = request.GET
+    try:
+        validate_price_and_brand(params)
+    except ValidationError as e:
+        return JsonResponse({"error": {'code': 'validation_error', 'message': list(e)[0]}}, status=403)
+    
+    brand = params.get('brand')
+    min_price = params.get('min_price')
+    laptops = Laptop.objects.filter(brand=brand)
+
     if not laptops:
-        return HttpResponseForbidden('No laptops with this params!')
-    return HttpResponse(json.dumps(get_dict_for_queryset(laptops)))
+        return JsonResponse({"error": {'code': 'validation_error', 'message': 'there is no this brand in store'}}, status=403)
+    laptops = laptops.filter(price__gte=min_price).order_by('price')
+
+    if not laptops:
+        return JsonResponse({"error": {'code': 'not_found', 'message': 'laptops not found'}}, status=404)
+    return JsonResponse(get_dict_for_queryset(laptops), safe=False)
 
 
-def last_laptop_details_view(request: HttpRequest) -> HttpResponse:
+def last_laptop_details_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть json-описание последнего созданного ноутбука.
     Если ноутбуков нет вообще, вернуть 404.
     """
     laptop = Laptop.objects.order_by('-created_at').first()
     if not laptop:
-        return HttpResponseNotFound('No laptops!')
-
-    return HttpResponse(json.dumps(laptop.to_json()))
+        return JsonResponse({"error": {'code': 'not_found', 'message': 'laptop not found'}}, status=404)
+    return JsonResponse(laptop.to_json())
--- challenges/views/level_2/b_blog.py
+++ challenges/views/level_2/b_blog.py
@@ -11,26 +11,38 @@
 """
 import json
 from datetime import datetime, timedelta
+from typing import Any
+from django.core.exceptions import ValidationError
 
 from django.db.models import QuerySet
-from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest
+from django.http import HttpRequest, JsonResponse
 
 from challenges.models import Blog
 
 
-def get_dict_for_queryset(queryset: QuerySet):
+def validate_last_days(days: str | None) -> None:
+    if days is None:
+        raise ValidationError('parameter "last_days" is missing')
+    try:
+        int(days)
+    except ValueError:
+        raise ValidationError('Parameter "last_days" should be integer')
+    
+
+def get_dict_for_queryset(queryset: QuerySet) -> list[dict[str, Any]]:
     data = [item.to_json() for item in queryset]
     return data
 
 
-def last_posts_list_view(request: HttpRequest) -> HttpResponse:
+def last_posts_list_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть 3 последних опубликованных поста.
     """
-    return HttpResponse(json.dumps(get_dict_for_queryset(Blog.objects.all()[:3])))
+    posts = Blog.objects.filter(published_at__isnull=False).order_by('-published_at')[:3]
+    return JsonResponse(get_dict_for_queryset(posts), safe=False)
 
 
-def posts_search_view(request: HttpRequest) -> HttpResponse:
+def posts_search_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть все посты, которые подходят под поисковый запрос.
     Сам запрос возьмите из get-параметра query.
@@ -40,56 +52,55 @@ def posts_search_view(request: HttpRequest) -> HttpResponse:
     text = request.GET.get('text')
 
     if not any([title, text]):
-        return HttpResponseBadRequest('Required params are missing!')
+        return JsonResponse({"error": {'code': 'validation_error', 'message': 'required parameters are missing'}}, status=400)
     
-    filter_params = {}
+    posts = Blog.objects.all()
     if title:
-        filter_params['title'] = title
+        posts = posts.filter(title__icontains=title)
     if text:
-        filter_params['text__icontains'] = text
+        posts = posts.filter(title__icontains=text)
 
-    posts = Blog.objects.filter(**filter_params)
-
-    return HttpResponse(json.dumps(get_dict_for_queryset(posts)))
+    return JsonResponse(get_dict_for_queryset(posts), safe=False)
 
 
-def untagged_posts_list_view(request: HttpRequest) -> HttpResponse:
+def untagged_posts_list_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть все посты без категории, отсортируйте их по автору и дате создания.
     """
     posts = Blog.objects.filter(category__isnull=True).order_by('author', '-created_at')
-    return HttpResponse(json.dumps(get_dict_for_queryset(posts)))
+    return JsonResponse(get_dict_for_queryset(posts), safe=False)
 
 
-def categories_posts_list_view(request: HttpRequest) -> HttpResponse:
+def categories_posts_list_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть все посты все посты, категория которых принадлежит одной из указанных.
     Возьмите get-параметр categories, в нём разделённый запятой список выбранных категорий.
     """
     categories_raw = request.GET.get('categories')
     if not categories_raw:
-        return HttpResponseBadRequest('Required params are missing!')
+        return JsonResponse({"error": {'code': 'validation_error', 'message': 'parametr "categories" is missing'}}, status=400)
     categories = categories_raw.split(',')
-    categories = [category.strip() for category in categories]
+    categories = [category.strip().capitalize() for category in categories]
+
     posts = Blog.objects.filter(category__in=categories)
 
-    return HttpResponse(json.dumps(get_dict_for_queryset(posts)))
+    return JsonResponse(get_dict_for_queryset(posts), safe=False)
 
 
-def last_days_posts_list_view(request: HttpRequest) -> HttpResponse:
+def last_days_posts_list_view(request: HttpRequest) -> JsonResponse:
     """
     В этой вьюхе вам нужно вернуть посты, опубликованные за последние last_days дней.
     Значение last_days возьмите из соответствующего get-параметра.
     """
     last_days = request.GET.get('last_days')
-    
-    if not last_days:
-        return HttpResponseBadRequest('Missing parametr "last_days"')
+
     try:
-        last_days = int(last_days)
-    except TypeError:
-        return HttpResponseBadRequest('Parametr "last_days" should be integer')
+        validate_last_days(last_days)
+    except ValidationError as e:
+        return JsonResponse({"error": {'code': 'validation_error', 'message': list(e)[0]}}, status=400)
+    
+    last_days = abs(int(last_days))
     datetime_now = datetime.now() - timedelta(days=last_days)
     posts = Blog.objects.filter(published_at__gte=datetime_now)
 
-    return HttpResponse(json.dumps(get_dict_for_queryset(posts)))
+    return JsonResponse(get_dict_for_queryset(posts), safe=False)
